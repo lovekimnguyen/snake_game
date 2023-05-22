dd6865e6d13c99c4a33f64c43cc158b9005eca1a
Hà Văn Nguyên <122628379+lovekimnguyen@users.noreply.github.com>
2023-05-22 13:18:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoard = document.getElementById('game-board');
+    const startButton = document.getElementById('start-button');
+    const scoreElement = document.getElementById('score');
+    const difficultySelect = document.getElementById('difficulty');
+    const snakeStyleSelect = document.getElementById('snake-style');
+
     let snake = [{x: 200, y: 200}];
     let food = {x: 100, y: 100};
     let dx = 0;
     let dy = 0;
+    let score = 0;
+    let gameSpeed = 200;
+
+    const snakeStyles = {
+        default: 'green',
+        realistic: 'url("snake.png")'
+    };
 
     function drawSnake() {
         gameBoard.innerHTML = '';
@@ -12,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             snakeElement.style.left = segment.x + 'px';
             snakeElement.style.top = segment.y + 'px';
             snakeElement.classList.add('snake');
+            snakeElement.style.backgroundImage = snakeStyles[snakeStyleSelect.value];
             gameBoard.appendChild(snakeElement);
         });
     }
@@ -28,10 +41,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const head = {x: snake[0].x + dx, y: snake[0].y + dy};
         snake.unshift(head);
         if (head.x === food.x && head.y === food.y) {
-            food = {
-                x: Math.floor(Math.random() * 20) * 20,
-                y: Math.floor(Math.random() * 20) * 20
-            };
+            food = getRandomFoodPosition();
+            score++;
+            scoreElement.textContent = 'Điểm số: ' + score;
+            updateGameSpeed();
         } else {
             snake.pop();
         }
@@ -80,8 +93,57 @@ document.addEventListener('DOMContentLoaded', () => {
         return (head.x < 0 || head.x >= gameBoard.clientWidth || head.y < 0 || head.y >= gameBoard.clientHeight);
     }
 
-    document.addEventListener('keydown', changeDirection);
-    const gameInterval = setInterval(gameLoop, 200);
+    function getRandomFoodPosition() {
+        const position = {
+            x: Math.floor(Math.random() * 20) * 20,
+            y: Math.floor(Math.random() * 20) * 20
+        };
+        if (isFoodOnSnake(position)) {
+            return getRandomFoodPosition();
+        }
+        return position;
+    }
+
+    function isFoodOnSnake(position) {
+        return snake.some(segment => segment.x === position.x && segment.y === position.y);
+    }
+
+    function updateGameSpeed() {
+        switch (difficultySelect.value) {
+            case 'easy':
+                gameSpeed = 200;
+                break;
+            case 'medium':
+                gameSpeed = 150;
+                break;
+            case 'hard':
+                gameSpeed = 100;
+                break;
+            default:
+                gameSpeed = 200;
+        }
+    }
+
+    function startGame() {
+        snake = [{x: 200, y: 200}];
+        food = getRandomFoodPosition();
+        dx = 0;
+        dy = 0;
+        score = 0;
+        scoreElement.textContent = 'Điểm số: 0';
+        gameSpeed = 200;
+        updateGameSpeed();
+        startButton.disabled = true;
+        document.addEventListener('keydown', changeDirection);
+        gameInterval = setInterval(gameLoop, gameSpeed);
+    }
+
+    function endGame() {
+        clearInterval(gameInterval);
+        startButton.disabled = false;
+        document.removeEventListener('keydown', changeDirection);
+        alert('Game over!');
+    }
 
     function gameLoop() {
         drawSnake();
@@ -89,8 +151,9 @@ document.addEventListener('DOMContentLoaded', () => {
         moveSnake();
 
         if (isCollision()) {
-            clearInterval(gameInterval);
-            alert('Game over!');
+            endGame();
         }
     }
+
+    startButton.addEventListener('click', startGame);
 });
